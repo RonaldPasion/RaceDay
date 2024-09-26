ee34942ed03558963ba826aa50f1fa4b4d2351cb
Ronald Pasion <52368740+RonaldPasion@users.noreply.github.com>
2024-09-25 17:08:49 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ let runnerAge = 0;
 if (runnerAge > 18 && registeredEarly) {
   raceNumber += 1000;
 }
+
+if (runnerAge > 18 && registeredEarly) {
+  console.log(`You will race at 9:30 am as number ${raceNumber}.`);
+}
