5548b83fe861c813eedca62e54c72fb8f8608a72
Ronald Pasion <52368740+RonaldPasion@users.noreply.github.com>
2024-09-25 17:16:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -12,4 +12,6 @@ if (runnerAge > 18 && registeredEarly) {
   console.log(`You will race at 11:00 am as number ${raceNumber}.`);
 } else if (runnerAge <18) {
   console.log(`Youth registrants run at 12:30 pm \(regardless of registration\). You are number ${raceNumber}`);
+} else {
+  console.log('Please visit the registration desk');
 }
